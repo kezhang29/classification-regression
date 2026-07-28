3b9ad1ffc726ab3525dd90f49f5ac45846d6a70b
kezhang29 <kevinzhang.oak@gmail.com>
2026-07-27 20:47:47 -0700

    Train model

--- binary-classification/keras_model.py
+++ binary-classification/keras_model.py
@@ -2,6 +2,7 @@ import os
 os.environ["KERAS_BACKEND"] = "torch" 
 
 import keras
+from keras import layers
 import torch
 from keras.datasets import imdb
 
@@ -9,6 +10,8 @@ from keras.datasets import imdb
 
 print(train_data)
 print(train_data.shape)
+print(train_labels)
+print(train_labels.shape)
 
 # word_index is a dictionary mapping words to an integer index.
 word_index = imdb.get_word_index()
@@ -31,8 +34,26 @@ def multi_hot_encode(data, *,num_words):
 
 training_data = multi_hot_encode(train_data, num_words = 10000)
 print(training_data[0])
+test_data = multi_hot_encode(test_data, num_words = 10000)
 
-model = keras.Sequential(
-    keras.layers.Dense(units=32,activation="reLu"),
-    keras.layers.Dense()
+train_labels = train_labels.astype("float32")
+test_labels = test_labels.astype("float32")
+
+x_train = training_data[10000:]
+x_val = training_data[:10000]
+y_train = test_labels[10000:]
+y_val = test_labels[:10000]
+
+model = keras.Sequential([
+    layers.Dense(units=16,activation="relu"),
+    layers.Dense(units=16,activation="relu"),
+    layers.Dense(units=1,activation="sigmoid")]
 )
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+history = model.fit(x_train,y_train,epochs=40,batch_size = 512,validation_data=(x_val,y_val))
